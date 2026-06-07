f975770c33ab105b6c39e1b1a5651db28446dbbc
Daniel H. <daniel.neves0021@gmail.com>
2026-06-07 11:08:48 -0300

    feat(rust): added borrowinf samples

--- rust_course/borrowing/src/main.rs
+++ rust_course/borrowing/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 32;
+    let &mut y: &i32 = &x;
+
+    println!("Here, {:p} exists.", y);
+
+    x = 12;
+    // println!("Heres, raises {:p} raises error.", y);
+    // Raises an error because the x value changes, now, borrow checker don't compile.
 }
